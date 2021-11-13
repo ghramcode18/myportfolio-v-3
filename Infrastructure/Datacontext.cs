38305fde81442fe37ffc7837cbefa3171f03a03e
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-11-13 20:06:18 +0200

    --

---
+++ Infrastructure/Datacontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Owner> owner { get; set; }
+    }
+}
